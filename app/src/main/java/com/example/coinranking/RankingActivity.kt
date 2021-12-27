4dbfd9441b293b1247e55671c6b648d89103d3a6
Nice <nicenicegame@gmail.com>
2021-12-27 18:26:49 +0700

    Create layout

--- app/src/main/java/com/example/coinranking/RankingActivity.kt
+++ app/src/main/java/com/example/coinranking/RankingActivity.kt
@@ -2,12 +2,15 @@ package com.example.coinranking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coinranking.databinding.ActivityRankingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RankingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityRankingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
